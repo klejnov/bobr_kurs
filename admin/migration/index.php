a7c5e17efe946d31a6b51841e6a1a67fc6d56f70
klejnov <petro.vich@list.ru>
2019-02-23 19:42:47 +0300

    class DB

--- admin/migration/index.php
+++ admin/migration/index.php
@@ -1,12 +1,5 @@
 <?php
 
-//$config = require "../config.php";
-//
-//$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['db_name'], $config['username'],
-//    $config['password'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
-//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
 require '../../classes/DataBase.php';
 
 $db = new DataBase();
